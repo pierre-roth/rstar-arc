5c904df5d9f72a3d3d118529d821397d70a359cb
pierre-roth <pitroth2003@gmail.com>
2025-03-04 19:32:07 +0100

    undo compute capability limitation

--- arc_baseline/run_baseline.sh
+++ arc_baseline/run_baseline.sh
@@ -13,11 +13,11 @@ TIME_LIMIT="" # No time limit by default
 VERSION=1  # Default to mark1.py
 TASK_INDEX=1
 MAX_ITERATIONS=5
-MODEL="Qwen/Qwen2.5-Coder-7B-Instruct"
+MODEL="Qwen/Qwen2.5-Coder-1.5B-Instruct"
 EVAL=false
 HINT=""
 VERBOSE=true
-DTYPE="bfloat16" # bfloat16 only supported in compute 8.0 and above otherwise use float16
+DTYPE="float16" # bfloat16 only supported in compute 8.0 and above otherwise use float16
 
 # Parse named command line arguments
 while [[ $# -gt 0 ]]; do
@@ -104,7 +104,7 @@ cat > "${TEMP_SCRIPT}" << EOL
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=${CPUS}
 #SBATCH --gres=gpu:${GPUS}
-#SBATCH --constraint='geforce_rtx_3090|a100|a6000'
+
 EOL
 
 # Add optional SBATCH parameters if provided
