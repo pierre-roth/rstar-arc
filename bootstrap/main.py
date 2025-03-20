3a1b7eef35b450c6d407a49feb1289fac3afa559
pierre-roth <pitroth2003@gmail.com>
2025-03-20 19:17:17 +0100

    fixed import error

--- bootstrap/main.py
+++ bootstrap/main.py
@@ -1,6 +1,12 @@
 import logging
 import os.path
+import sys
 from vllm import LLM, SamplingParams
+
+# Get the parent directory
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+# Add the parent directory to sys.path
+sys.path.append(parent_dir)
 
 from prompt import get_bootstrap_prompt
 from config import Config, DEFAULT_DATA_SAMPLE_PATH
