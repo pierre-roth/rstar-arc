47e5712b0b8f41c2e594eef7a2300619e87f4f6d
pierre-roth <pitroth2003@gmail.com>
2025-03-28 17:52:25 +0100

    deleted redundant agents and configs
    added files for policy and reward model training

--- rstar_deepthink/agents/__init__.py
+++ rstar_deepthink/agents/__init__.py
@@ -1,8 +1,6 @@
 from .base_agent import Agent
 from .beam_search import BS
 from .mcts import MCTS
-from .pw_mcts import PWMCTS
-from .similiarity_mcts import SMCTS
 from .custom import Custom
 
-__all__ = ["Agent", "BS", "MCTS", "PWMCTS", "SMCTS", "Custom"]
+__all__ = ["Agent", "BS", "MCTS", "Custom"]
--- train/save_sft_data.py
+++ train/save_sft_data.py
@@ -3,4 +3,3 @@ from rstar_deepthink.node import Node
 
 
 def save_sft(config: Config, nodes: list[Node]):
-    pass
