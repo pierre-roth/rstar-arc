7ea1b464fa7f458c48a407afebf367ac1a47d7eb
pierre-roth <pitroth2003@gmail.com>
2025-04-08 14:23:30 +0200

    update coverage calculating script

--- train/calculate_coverage.py
+++ train/calculate_coverage.py
@@ -22,18 +22,27 @@ def calculate_coverage(round_num: int):
                 solution = json.loads(line)
                 task_names.add(solution["task_name"] + ".json")
 
+    # per folder covered tasks
+    folders = {}
+
     # go through every directory in task path and calculate coverage for each one
     for dir_name in os.listdir(task_path):
         dir_path = os.path.join(task_path, dir_name)
         if os.path.isdir(dir_path):
             total_tasks = 0
             covered_tasks = 0
+
+            folders[dir_name] = set()
             for task_file in os.listdir(dir_path):
                 if task_file.endswith(".json"):
                     total_tasks += 1
                     if task_file in task_names:
+                        folders[dir_name].add(task_file)
                         covered_tasks += 1
             print(f"{dir_name}: {covered_tasks}/{total_tasks} = {covered_tasks / total_tasks:.2%} coverage")
+
+    print(folders.get("training2", set()).difference(folders.get("training", set())))
+    print(f"Total tasks: {len(task_names)}")
 
 
 if __name__ == "__main__":
