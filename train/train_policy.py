c3f35b3a88447b6997aead89011383eea6149d0b
pierre-roth <pitroth2003@gmail.com>
2025-04-14 11:21:38 +0200

    larger context window

--- train/train_policy.py
+++ train/train_policy.py
@@ -47,7 +47,7 @@ MODEL_ID = "Qwen/Qwen2.5-Coder-1.5B"
 TRAINING_DATASET_PATH = os.path.join(NET_SCRATCH_PATH, "sft_data", f"round_{1}", "dataset_training.jsonl")
 VALIDATION_DATASET_PATH = os.path.join(NET_SCRATCH_PATH, "sft_data", f"round_{1}", "dataset_validation.jsonl")
 OUTPUT_DIR = os.path.join(NET_SCRATCH_PATH, "models", "fine_tuned", "policy", f"fine-tuned-{MODEL_ID.split('/')[1]}")
-MAX_SEQ_LENGTH = 8192  # Adjust based on your data and GPU memory
+MAX_SEQ_LENGTH = 10240  # Adjust based on your data and GPU memory
 WANDB_PROJECT = "deepthink-sft"  # Added wandb project name
 WANDB_ENTITY = None  # Set to your team name or username if needed
 
